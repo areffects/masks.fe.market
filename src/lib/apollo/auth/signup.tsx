dac6c235ecc395db1afc03733b79bbe1bd282f8f
Volodymyr Saakian <vsaakian@lohika.com>
2020-09-25 14:32:41 +0300

    Update auth

--- src/lib/apollo/auth/signup.tsx
+++
@@ -1,7 +0,0 @@
-// import React, { ReactElement } from 'react'
-
-// const SignUp = (): ReactElement => {
-// 	return <div>Hello SignUp</div>
-// }
-
-// export default SignUp
---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react'
+
+const SignUp = (): ReactElement => {
+	return <div>Hello SignUp</div>
+}
+
+export default SignUp
