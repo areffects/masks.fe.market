d06b5cc08ed9325cdbd6b9818145a39c4d7e4fb7
Volodymyr Saakian <vsaakian@lohika.com>
2020-09-26 21:48:35 +0300

    Delete all errors

--- src/pages/profile/user/index.tsx
+++ src/pages/profile/user/index.tsx
@@ -5,12 +5,10 @@ import { IS_LOGGED_IN } from 'src/constants/graphql/queries'
 import { PRODUCTS } from 'src/constants/paths'
 
 const User = (): ReactElement => {
-	const {
-		data: { isLoggedIn },
-	} = useQuery(IS_LOGGED_IN)
+	const { data } = useQuery(IS_LOGGED_IN)
 	const router = useRouter()
 	useEffect(() => {
-		if (!isLoggedIn) {
+		if (!data.isLoggedIn) {
 			router.push(PRODUCTS)
 		}
 	}, [])
