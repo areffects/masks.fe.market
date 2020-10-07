10bd03c2ec9febdecd91caf3a2a03b6d45b48507
Volodymyr Saakian <vsaakian@lohika.com>
2020-10-07 17:27:23 +0300

    Update

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import nameToFile from './files'
 import React from 'react'
 import styled from 'styled-components'
 
-interface Props extends React.FC {
+interface Props {
 	name: string
 }
 const IconWrapper = styled.div`
